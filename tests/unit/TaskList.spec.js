2e719e0a5e038be077007f40db5f23f8c8963d44
Gary Neo <gary.neo@ufinity.com>
2020-04-17 11:20:42 +0800

    taskbox UI

---
+++ tests/unit/TaskList.spec.js
@@ -0,0 +1,18 @@
+import Vue from "vue";
+import PureTaskList from "@/components/PureTaskList.vue";
+import { withPinnedTasksData } from "@/components/PureTaskList.stories";
+
+describe("TaskList", () => {
+  it("renders pinned tasks at the start of the list", () => {
+    const Constructor = Vue.extend(PureTaskList);
+    const vm = new Constructor({
+      propsData: { tasks: withPinnedTasksData }
+    }).$mount();
+    const firstTaskPinned = vm.$el.querySelector(
+      ".list-item:nth-child(1).TASK_PINNED"
+    );
+
+    // We expect the pinned task to be rendered first, not at the end
+    expect(firstTaskPinned).not.toBe(null);
+  });
+});
